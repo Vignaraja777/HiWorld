e8ef079df46f8f0ce7a8f1f53b967256054e3bae
vignaraja <vignaraja8@gmail.com>
2021-06-02 21:09:33 +0530

    First Commit

---
+++ src/test/java/com/example/HiWorld/HiWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HiWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
